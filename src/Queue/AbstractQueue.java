51852e2d84dc2ee01a12e528af5f82f670054e56
taojiatao <245915794@qq.com>
2016-08-23 21:01:06 +0800

    src

---
+++ src/Queue/AbstractQueue.java
@@ -0,0 +1,59 @@
+package Queue;
+
+import java.util.*;
+
+public abstract class AbstractQueue<E>
+        extends AbstractCollection<E>
+        implements java.util.Queue<E> {
+
+    protected AbstractQueue() {
+    }
+
+    public boolean add(E e) {
+        if (offer(e))
+            return true;
+        else
+            throw new IllegalStateException("Queue full");
+    }
+
+    public E remove() {
+        E x = poll();
+        if (x != null)
+            return x;
+        else
+            throw new NoSuchElementException();
+    }
+
+    public E element() {
+        E x = peek();
+        if (x != null)
+            return x;
+        else
+            throw new NoSuchElementException();
+    }
+
+    public void clear() {
+        while (poll() != null)
+            ;
+    }
+
+    public boolean addAll(Collection<? extends E> c) {
+        if (c == null)
+            throw new NullPointerException();
+        if (c == this)
+            throw new IllegalArgumentException();
+        boolean modified = false;
+        for (E e : c)
+            if (add(e))
+                modified = true;
+        return modified;
+    }
+    //:offer
+    //add:
+
+    //element:Retrieves, but does not remove, the head of this queue 和peek唯一区别 element为空抛出异常
+    //peek:Retrieves, but does not remove, the head of this queue 和element唯一区别 peek为空返回null
+
+    //poll:Retrieves and removes the head of this queue 抛异常
+    //remove:Retrieves and removes the head of this queue 返回null
+}
---
+++ src/stack/LinkedStack.java
@@ -0,0 +1,92 @@
+package stack;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by taojiatao.aron on 2016/8/23.
+ */
+public class LinkedStack < E > implements Iterable< E > {
+    private Node< E > first;
+    private int size;
+
+    private static class Node < E > {
+        E item;
+        Node next;
+    }
+
+    public LinkedStack () {
+        first = null;
+        size = 0;
+    }
+
+    public void push ( E item ) {
+        Node< E > oldFirst = first;
+        first = new Node<>();
+        first.item = item;
+        first.next = oldFirst;
+        size++;
+    }
+
+    //要是数组实现这里就要考虑内存泄露了 用链表不用担心
+    public E pop () {
+        E item = peek();
+        first = first.next;
+        size--;
+        return item;
+    }
+
+    public E peek () {
+        if ( isEmpty() ) {
+            throw new NoSuchElementException( "Stack underflow" );
+        }
+        return first.item;
+    }
+
+    public boolean isEmpty () {
+        return first == null;
+    }
+
+    public int size () {
+        return size;
+    }
+
+    @Override
+    public String toString () {
+        StringBuilder stringBuilder = new StringBuilder();
+        for ( E item : this ) {
+            stringBuilder.append( item + " " );
+        }
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public Iterator< E > iterator () {
+        return new ListIterator<>( first );
+    }
+
+    private class ListIterator < E > implements Iterator< E > {
+        private Node< E > current;
+
+        public ListIterator ( Node< E > first ) {
+            this.current = first;
+        }
+
+        public boolean hasNext () {
+            return current != null;
+        }
+
+        public void remove () {
+            throw new UnsupportedOperationException();
+        }
+
+        public E next () {
+            if (!hasNext() ) {
+                if (!hasNext()) throw new NoSuchElementException();
+            }
+            E item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+}
